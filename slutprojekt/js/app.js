f3666cfb89a641f65855e9b4563e5d7111773e06
Alabin07 <a.folketoft@gmail.com>
2025-04-02 14:14:20 +0200

    Fett med progress, cool navbar, responsiv, exempelsida med boids

--- slutprojekt/js/app.js
+++ slutprojekt/js/app.js
@@ -13,3 +13,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenELements = document.querySelectorAll(".hidden");
 hiddenELements.forEach((el) => observer.observe(el));
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+});
